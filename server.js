ec8c659d0d2d42042001c16e96e388e03e318dd6
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-09 20:59:35 +0100

    WIP: setup GET endpoint to get all accounts from database

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  db.select('*')
+    .from('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts)
+    }).catch(err => {
+      res.json(err)
+    })
+})
+
 module.exports = server;
