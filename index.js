7e54f1779ca8f70cd719926649a8d631a82051f1
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-09 21:27:32 +0100

    Completed MVP: refactored accounts routes to seperate endpoints

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const server = require('./server.js');
 
+const accountsRoutes = require('./Accounts/accountsRouter.js')
+
+server.use('/api/accounts', accountsRoutes)
+
+server.use('/', (req, res) => res.send('API is up...'))
+
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
